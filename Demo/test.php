466463b92a96b29386ca065831530fd1e33fd0f3
宇润 <admin@yurunsoft.com>
2017-04-17 16:06:39 +0800

    更新说明和Demo

--- Demo/test.php
+++ Demo/test.php
@@ -3,7 +3,13 @@ namespace Yurun\Until;
 require '../HttpRequest.class.php';
 require '../HttpResponse.class.php';
 $h = HttpRequest::newSession();
+// 设置单个header
 $h->header('test','666');
+// 设置多个header
+$h->header(array(
+	'test2'	=>	'777',
+	'test3'	=>	'888',
+));
 // socks4代理
 $h->proxy('127.0.0.1',1080,'socks4');
 // socks5代理
@@ -12,5 +18,20 @@ $h->proxy('127.0.0.1',1080,'socks5');
 $h->proxy('124.88.67.83',843);
 // 取消使用代理
 $h->useProxy = false;
-$r = $h->get('http://www.baidu.com');
+// 设置POST内容方法1
+$h->content('123');
+// 设置POST内容方法2
+$h->params(array(
+	'id'	=>	2,
+));
+// cURL设置多个
+$h->options(array(
+	CURLOPT_HEADER	=>	true,
+));
+// cURL设置单个
+$h->option(CURLOPT_HEADER,true);
+// POST请求+POST参数
+$r = $h->post('http://www.baidu.com/s',array('wd'=>'搜索词'));
+// GET请求+GET参数+失败重试3次
+$r = $h->retry(3)->get('http://www.baidu.com/s',array('wd'=>'搜索词'));
 var_dump($r->body,$r->headers,$r->cookies);
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 #YurunHttp
+
+##简介
+
+一个基于PHP cURL的开源HTTP类库，支持链式操作，省去繁杂的cURL使用方法。
+
+具体详见Demo
