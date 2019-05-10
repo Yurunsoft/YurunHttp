d4305bd111b79774424f74fcde752db013f738f2
宇润 <admin@yurunsoft.com>
2019-05-10 16:26:19 +0800

    修复设置timeout无效问题

--- src/YurunHttp/Handler/Swoole.php
+++ src/YurunHttp/Handler/Swoole.php
@@ -101,12 +101,6 @@ class Swoole implements IHandler
                 $this->parseSSL();
                 $this->parseProxy();
                 $this->parseNetwork();
-                // 设置客户端参数
-                if(!empty($this->settings))
-                {
-                    $this->settings['timeout'] = 30;
-                    $this->handler->set($this->settings);
-                }
                 // 发送
                 $path = $uri->getPath();
                 if('' === $path)
