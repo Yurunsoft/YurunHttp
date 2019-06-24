93e41f3cb708000833d30904907979c2a1e2a512
Yurun <admin@yurunsoft.com>
2019-06-24 17:34:51 +0800

    更新 travis/install.php

--- travis/install.php
+++ travis/install.php
@@ -6,7 +6,7 @@ $nproc = Args::get('nproc');
 $versionName = Args::get('version-name');
 
 define('PHP_70_SWOOLE_VERSION', 'v4.3.5');
-define('OTHER_SWOOLE_VERSION', 'v4.3.5');
+define('OTHER_SWOOLE_VERSION', 'v4.4.0-beta');
 
 if(version_compare(PHP_VERSION, '7.0', '>='))
 {
