f09f7d37df727b041b4dda10adc912e089f41fa2
Yurun <admin@yurunsoft.com>
2019-06-24 17:05:54 +0800

    测试脚本将 PHP 7 的 Swoole 版本强制设为 4.3.5，因不再兼容

--- travis/install.php
+++ travis/install.php
@@ -5,9 +5,17 @@ Args::init();
 $nproc = Args::get('nproc');
 $versionName = Args::get('version-name');
 
+define('PHP_70_SWOOLE_VERSION', '4.3.5');
+define('OTHER_SWOOLE_VERSION', '4.3.5');
+
 if(version_compare(PHP_VERSION, '7.0', '>='))
 {
-    `wget https://github.com/swoole/swoole-src/archive/v4.4.0-beta.tar.gz -O swoole.tar.gz && mkdir -p swoole && tar -xf swoole.tar.gz -C swoole --strip-components=1 && rm swoole.tar.gz && cd swoole && phpize && ./configure && make -j{$nproc} && make install && cd -`;
+    $version = OTHER_SWOOLE_VERSION;
+    if(version_compare(PHP_VERSION, '7.1', '<'))
+    {
+        $version = PHP_70_SWOOLE_VERSION;
+    }
+    `wget https://github.com/swoole/swoole-src/archive/{$version}.tar.gz -O swoole.tar.gz && mkdir -p swoole && tar -xf swoole.tar.gz -C swoole --strip-components=1 && rm swoole.tar.gz && cd swoole && phpize && ./configure && make -j{$nproc} && make install && cd -`;
 
     `echo "extension = swoole.so" >> ~/.phpenv/versions/{$versionName}/etc/php.ini`;
 }
