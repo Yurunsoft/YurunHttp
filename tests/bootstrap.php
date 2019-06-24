c8affd85c6e6b1f7192d172d9b08db6b64c42626
Yurun <admin@yurunsoft.com>
2019-06-24 10:28:42 +0800

    提交

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,10 +3,10 @@ require dirname(__DIR__) . '/vendor/autoload.php';
 
 // start server
 $cmd = __DIR__ . '/server/start-server.sh';
-`{$cmd}`;
+echo `{$cmd}`, PHP_EOL;
 
 register_shutdown_function(function(){
     // stop server
     $cmd = __DIR__ . '/server/stop-server.sh';
-    `{$cmd}`;
+    echo `{$cmd}`, PHP_EOL;
 });
