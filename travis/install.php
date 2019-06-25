8b2fc8d264a364219f2eba74c132f45836fe9cea
Yurun <admin@yurunsoft.com>
2019-06-25 10:48:07 +0800

    修复测试脚本兼容性

--- travis/install.php
+++ travis/install.php
@@ -5,8 +5,9 @@ Args::init();
 $nproc = Args::get('nproc');
 $versionName = Args::get('version-name');
 
-define('PHP_70_SWOOLE_VERSION', 'v4.3.5');
 define('OTHER_SWOOLE_VERSION', 'v4.4.0-beta');
+define('PHP_70_SWOOLE_VERSION', 'v4.3.5');
+define('PHP_73_SWOOLE_VERSION', 'ed77b8b48fd113dad9bc51dc348a51aec51b220d');
 
 if(version_compare(PHP_VERSION, '7.0', '>='))
 {
@@ -14,6 +15,10 @@ if(version_compare(PHP_VERSION, '7.0', '>='))
     if(version_compare(PHP_VERSION, '7.1', '<'))
     {
         $version = PHP_70_SWOOLE_VERSION;
+    }
+    else if(version_compare(PHP_VERSION, '7.3', '>=') && version_compare(PHP_VERSION, '7.4', '<'))
+    {
+        $version = PHP_73_SWOOLE_VERSION;
     }
     `wget https://github.com/swoole/swoole-src/archive/{$version}.tar.gz -O swoole.tar.gz && mkdir -p swoole && tar -xf swoole.tar.gz -C swoole --strip-components=1 && rm swoole.tar.gz && cd swoole && phpize && ./configure && make -j{$nproc} && make install && cd -`;
 
