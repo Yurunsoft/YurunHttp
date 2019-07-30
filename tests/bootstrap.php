df9b1dd5d7b52b4abb23f6f47fd2489298aeb05b
Yurun <admin@yurunsoft.com>
2019-07-30 14:47:17 +0800

    修复低版本 PHP 测试用例

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,6 +1,8 @@
 <?php
 require dirname(__DIR__) . '/vendor/autoload.php';
 
+define('SWOOLE_ON', version_compare(PHP_VERSION, '7.0', '>='));
+
 function testEnv($name, $default = null)
 {
     $result = getenv($name);
@@ -34,28 +36,31 @@ else
     throw new \RuntimeException('Http server start failed');
 }
 
-// WebSocket Server
-$cmd = __DIR__ . '/server/WebSocket/bin/start.sh';
-echo 'Starting WebSocket server...', PHP_EOL;
-echo `{$cmd}`, PHP_EOL;
-$serverStarted = false;
-for($i = 0; $i < 10; ++$i)
+if(SWOOLE_ON)
 {
-    @file_get_contents(str_replace('ws://', 'http://', testEnv('WS_SERVER_HOST', 'ws://127.0.0.1:8900/')));
-    if(isset($http_response_header) && 'HTTP/1.1 400 Bad Request' === $http_response_header[0])
+    // WebSocket Server
+    $cmd = __DIR__ . '/server/WebSocket/bin/start.sh';
+    echo 'Starting WebSocket server...', PHP_EOL;
+    echo `{$cmd}`, PHP_EOL;
+    $serverStarted = false;
+    for($i = 0; $i < 10; ++$i)
     {
-        $serverStarted = true;
-        break;
+        @file_get_contents(str_replace('ws://', 'http://', testEnv('WS_SERVER_HOST', 'ws://127.0.0.1:8900/')));
+        if(isset($http_response_header) && 'HTTP/1.1 400 Bad Request' === $http_response_header[0])
+        {
+            $serverStarted = true;
+            break;
+        }
+        sleep(1);
     }
-    sleep(1);
-}
-if($serverStarted)
-{
-    echo 'WebSocekt server started!', PHP_EOL;
-}
-else
-{
-    throw new \RuntimeException('WebSocekt server start failed');
+    if($serverStarted)
+    {
+        echo 'WebSocekt server started!', PHP_EOL;
+    }
+    else
+    {
+        throw new \RuntimeException('WebSocekt server start failed');
+    }
 }
 
 register_shutdown_function(function(){
@@ -65,8 +70,11 @@ register_shutdown_function(function(){
     echo `{$cmd}`, PHP_EOL;
     echo 'Http Server stoped!', PHP_EOL;
 
-    $cmd = __DIR__ . '/server/WebSocket/bin/stop.sh';
-    echo 'Stoping WebSocket server...', PHP_EOL;
-    echo `{$cmd}`, PHP_EOL;
-    echo 'WebSocket Server stoped!', PHP_EOL;
+    if(SWOOLE_ON)
+    {
+        $cmd = __DIR__ . '/server/WebSocket/bin/stop.sh';
+        echo 'Stoping WebSocket server...', PHP_EOL;
+        echo `{$cmd}`, PHP_EOL;
+        echo 'WebSocket Server stoped!', PHP_EOL;
+    }
 });
