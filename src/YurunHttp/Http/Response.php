fd2fea6223221bae04d10b81485534bf06ed63a0
Yurun <admin@yurunsoft.com>
2019-11-07 15:24:14 +0800

    修复类不存在问题

--- src/YurunHttp/Http/Response.php
+++ src/YurunHttp/Http/Response.php
@@ -1,6 +1,7 @@
 <?php
 namespace Yurun\Util\YurunHttp\Http;
 
+use Yurun\Util\YurunHttp\Http\Psr7\Consts\StatusCode;
 use Yurun\Util\YurunHttp\Http\Psr7\Response as Psr7Response;
 
 class Response extends Psr7Response
