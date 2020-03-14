6b0b03c2a19ffc1edcd92bf74913e8cf454ebbdb
Yurun <admin@yurunsoft.com>
2020-03-14 14:34:39 +0800

    优化 Http2 客户端

--- src/YurunHttp/Http2/SwooleClient.php
+++ src/YurunHttp/Http2/SwooleClient.php
@@ -72,6 +72,13 @@ class SwooleClient implements IHttp2Client
     private $timeout;
 
     /**
+     * 接收协程ID
+     *
+     * @var int
+     */
+    private $recvCo;
+
+    /**
      * @param string $host
      * @param int $port
      * @param bool $ssl
@@ -109,7 +116,6 @@ class SwooleClient implements IHttp2Client
                     'timeout'   =>  $this->timeout,
                 ]);
             }
-            $this->startRecvCo();
             return true;
         }
         else
@@ -212,6 +218,10 @@ class SwooleClient implements IHttp2Client
      */
     public function recv($streamId = -1, $timeout = null)
     {
+        if(!$this->recvCo || !Coroutine::exists($this->recvCo))
+        {
+            $this->startRecvCo();
+        }
         if(isset($this->recvChannels[$streamId]))
         {
             $channel = $this->recvChannels[$streamId];
@@ -261,10 +271,10 @@ class SwooleClient implements IHttp2Client
         {
             return false;
         }
-        return Coroutine::create(function(){
+        return $this->recvCo = Coroutine::create(function(){
             while($this->isConnected())
             {
-                $swooleResponse = $this->http2Client->recv();
+                $swooleResponse = $this->http2Client->recv($this->timeout);
                 if(!$swooleResponse)
                 {
                     $this->close();
