d113bfbe0323ddfda1309b7f03b0bd1a9d9a6047
Yurun <admin@yurunsoft.com>
2020-12-14 16:01:10 +0800

    修复测试

--- tests/server/Http/server.php
+++ tests/server/Http/server.php
@@ -4,7 +4,7 @@ use Workerman\Protocols\Http\Request;
 use Workerman\Protocols\Http\Response;
 use Workerman\Connection\TcpConnection;
 
-require_once dirname(__DIR__, 3) . '/vendor/autoload.php';
+require_once dirname(dirname(dirname(__DIR__))) . '/vendor/autoload.php';
 require_once __DIR__ . '/WorkermanHttp.php';
 
 // #### http worker ####
