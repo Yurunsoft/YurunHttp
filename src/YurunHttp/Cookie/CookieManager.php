d422f97065ae84a19a4718a8a40a28c7be592712
Yurun <admin@yurunsoft.com>
2026-06-29 20:07:24 +0800

    修复低版本 PHP 兼容性

--- src/YurunHttp/Cookie/CookieManager.php
+++ src/YurunHttp/Cookie/CookieManager.php
@@ -293,7 +293,7 @@ class CookieManager
                     $path = \dirname($path);
                 }
             }
-            if ('\\' === \DIRECTORY_SEPARATOR && str_contains($path, \DIRECTORY_SEPARATOR))
+            if ('\\' === \DIRECTORY_SEPARATOR && false !== strpos($path, \DIRECTORY_SEPARATOR))
             {
                 $path = str_replace(\DIRECTORY_SEPARATOR, '/', $path);
             }
